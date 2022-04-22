bade6e7f254574e4b8d4be4ee601ee04b2d94c54
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-04-21 22:17:15 -0400

    Added compiler source code
    Added initial compiler source code.

---
+++ Compiler/include/toAssembly.hpp
@@ -0,0 +1,9 @@
+#ifndef IFUCKEDYOURMOMSCRIPT_ASSEMBLY_COMPILER_HPP
+#define IFUCKEDYOURMOMSCRIPT_ASSEMBLY_COMPILER_HPP
+
+#include <string>
+
+std::string fileToAssembly(std::string content);
+std::string commandToAssembly(std::string command);
+
+#endif

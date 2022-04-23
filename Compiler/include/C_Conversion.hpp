793f2aa98867cdb8befd4a5a2c0e7e5e40dded43
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-04-22 23:53:44 -0400

    Added C_Conversion code
    Added C_Conversion source file and header

---
+++ Compiler/include/C_Conversion.hpp
@@ -0,0 +1,9 @@
+#ifndef IFUCKEDYOURMOMSCRIPT_C_CONVERTER_HPP
+#define IFUCKEDYOURMOMSCRIPT_C_CONVERTER_HPP
+
+#include <string>
+
+void fileToC(std::string content);
+std::string commandToC(char command);
+
+#endif
---
+++ Compiler/src/C_Conversion.cpp
@@ -0,0 +1,76 @@
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include "C_Conversion.hpp"
+
+char commands[] = { '-', '+', '*', '\\', '>', '<', '(', ')', '{', '}', '@', '$', '!', '?', 'a', 'b', 'c', 'd', 'e', 'f', 'A', 'B', 'C', 'D', 'E', 'F' };
+
+std::string c_commands[] =
+{
+    "ptr[offset]--;",
+    "ptr[offset]++;",
+    "ptr[offset] *= ptr[offset+1];",
+    "ptr[offset] = ptr[offset]/ptr[offset+1];",
+    "offset++;",
+    "offset--;",
+    "while(ptr[offset] != 0){",
+    "}",
+    "if(ptr[offset] == 0){",
+    "}",
+    "putchar(ptr[offset]);",
+    "ptr[offset] = getchar();",
+    "ptr[offset] <<= ptr[offset+1];",
+    "ptr[offset] >>= ptr[offset-1];",
+    "int *segfaultPtr = 0; *segfaultPtr = 0xDEADBEEF;",
+    "a = ptr[offset];",
+    "b = ptr[offset];",
+    "c = ptr[offset];",
+    "d = ptr[offset];",
+    "e = ptr[offset];",
+    "f = ptr[offset];",
+    "ptr[offset] = a;",
+    "ptr[offset] = b;",
+    "ptr[offset] = c;",
+    "ptr[offset] = d;",
+    "ptr[offset] = e;",
+    "ptr[offset] = f;"
+};
+
+std::string fileTemplate = "#include <stdlib.h>\n#include <stdio.h>\n#include <string.h>\nint a = 0xDEADBEEF; int b = 0xDEADBEEF; int c = 0xDEADBEEF; int d = 0xDEADBEEF; int e = 0xDEADBEEF; int f = 0xDEADBEEF; int *ptr = (int*)malloc(4000); int offset = 0;\nint %s(void){%s}";
+
+void fileToC(std::string content, std::string filename)
+{
+    FILE *fileC;
+    std::string functionName;
+    std::string translation;
+    int x = 0;
+    // Extract function name from file
+    while(content[x] =! '\n')
+    {
+        functionName += content[x];
+        x++;
+    }
+    content.erase(0, x);
+    // Translate the code to C
+    for(int i = 0; i < content.length(); i++)
+    {
+        translation += commandToC(content[i]);
+    }
+    if(functionName.c_str() == "main")
+        translation += "main();";
+    else
+        translation += "return f;";
+    fileC = fopen(filename.c_str(), "w");
+    // Write the file
+    fprintf(fileC, fileTemplate.c_str(), functionName.c_str(), translation.c_str());
+    // Close the file
+    fclose(fileC);
+}
+std::string commandToC(char command)
+{
+    for(int i = 0; i < 26; i++)
+        if(strcmp(&command, &commands[i]) == 0)
+            return c_commands[i];
+    std::string ret = "";
+    return ret;
+}

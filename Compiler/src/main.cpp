3bc29ea8a0dd60a81a3adf316f41544adb8ad78c
Teresa Fuckyouiamnotputingmylastnameheresuckmycock <99054975+ComradeYellowCitrusFruit@users.noreply.github.com>
2022-04-23 23:28:26 -0400

    Added an actual main function
    Added an actual main function, and arguments.

--- Compiler/src/main.cpp
+++ Compiler/src/main.cpp
@@ -1,6 +1,63 @@
 #include <iostream>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include "Compiler\include\C_Conversion.hpp"
+
+std::string args[] = 
+{ 
+                "-i",
+                "--input",
+                "-o",
+                "--output",
+                "-s",
+                "--segfault",
+                "-nc",
+                "--no-c"
+};
 
 int main(int argc, char **argv)
 {
-    
+    std::string inputFileName;
+    std::string outputFileName = "out.c";
+    std::string Content; 
+    FILE *inputFile = fopen(inputFileName.c_str(), "r");
+    char *contentBuffer;
+    bool segfault = false;
+    bool nc = false;
+    std::cout << "Processing Arguments...\n";
+    for(int i = 0; i < argc; i++)
+    {
+        if(strcmp(argv[i], args[0].c_str()) == 0 || strcmp(argv[i], args[1].c_str()) == 0)
+        {
+            inputFileName += argv[i+1];
+        }
+        else if(strcmp(argv[i], args[2].c_str()) == 0 || strcmp(argv[i], args[3].c_str()) == 0)
+        {
+            outputFileName.clear();
+            outputFileName += argv[i+1];
+        }
+        else if(strcmp(argv[i], args[4].c_str()) == 0 || strcmp(argv[i], args[5].c_str()) == 0)
+        {
+            segfault = true;
+        }
+        else if(strcmp(argv[i], args[6].c_str()) == 0 || strcmp(argv[i], args[7].c_str()) == 0)
+        {
+            nc = true;
+        }
+    }
+    std::cout <<  "Reading input file...\n";
+    fscanf(inputFile, "%s", contentBuffer);
+    std::cout << "Reading input file...\n";
+    Content +=  *contentBuffer;
+    if(segfault) Content += '~';
+    fileToC(Content, outputFileName, nc);
+    fclose (inputFile);
+    if(segfault)
+    {
+        std::cout << "Exiting by triggering a segfault, have fun on the other side...\n";
+        printf("This process is %X...\n", 0xDEADBEEF);
+        int *ptr = 0;
+        *ptr = 0xDEADBEEF;
+    }
 }
